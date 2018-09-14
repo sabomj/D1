655ccd89d8f009ffbd9adae14ad8a2f4237219b5
Madi Sabo <sabomj@umich.edu>
2018-09-14 14:30:38 -0400

    changes to challenges.py

--- challenges.py
+++ challenges.py
@@ -1,3 +1,5 @@
+
+
 from flask import Flask, render_template, request
 import requests
 import json
@@ -5,11 +7,11 @@ import json
 app = Flask(__name__)
 
 
-#Task 2 : Dynamic URLS 
+#Task 2 : Dynamic URLS
     #edit the view function to display 'Welcome to <course_name>' on localhost:5000/course/<course>
 @app.route('/course/<course_name>')
-def courseView():
-    return ""
+def courseView(course_name):
+    return "Welcome to " + course_name
 
 #Task 3.1 Basic HTML Form
     #Set the method and action of the HTML form, such that form data is sent to /result using POST method
@@ -19,7 +21,12 @@ def formView():
     html_form = '''
     <html>
     <body>
-    <form>
+    <form action ='/result' method = "POST">
+        <label> Enter an ingredient : </label>
+        <br>
+        <input type='text' name ='ingredient'></input>
+        <input type='submit' name ='Submit'></input>
+
     </form>
     </body>
     </html>
@@ -29,13 +36,14 @@ def formView():
 #Task 3.2 : Processing Form Data
 @app.route('/result', methods = ['GET', 'POST'])
 def resultView():
-    # Make an API request to Recipe API for the ingredient entered in the form and display the recipe results 
-    #Step 1 : Receive the ingredient from the form if request type is POST
-    #Step 2 : Create paramaters JSON with the ingredient received in step 1 in the form required by http://www.recipepuppy.com/about/api/
-    #Step 3 : Make an API request to Recipe API and parameters in Step 2 
-    #Step 4 : Parse the response from API request in JSON 
-    #Step 5 : Display the response in browser (remember : HTML takes only strings)
-    return ""
+    if request.method == "POST":
+        ingredient = request.form.get("ingredient")
+        params = {}
+        params["i"] = ingredient
+        response = requests.get("http://www.recipepuppy.com/api/", params = params)
+        response_text = json.loads(response.text)
+    return str(response_text)
+
 
 
 if __name__ == '__main__':
